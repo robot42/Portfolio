b5283338f8c711b3271e6be8cea9157e3b8806e9
robot42 <github@ehuettig42.de>
2018-02-24 22:53:50 +0100

    1

--- Cpp/CppPlayground/CppPlayground/main.cpp
+++ Cpp/CppPlayground/CppPlayground/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  CppPlayground
-//
-//  Created by rico on 24.02.18.
-//  Copyright © 2018 rico. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[])
+{
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
