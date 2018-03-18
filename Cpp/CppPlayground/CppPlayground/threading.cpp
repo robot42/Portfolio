0ec11074cf25262a5e62a1fc3e269b033dc0ec9a
robot42 <github@ehuettig42.de>
2018-03-18 17:47:18 +0100

    test

---
+++ Cpp/CppPlayground/CppPlayground/threading.cpp
@@ -0,0 +1,9 @@
+//
+//  threading.cpp
+//  CppPlayground
+//
+//  Created by rico on 18.03.18.
+//  Copyright © 2018 rico. All rights reserved.
+//
+
+#include "threading.hpp"
---
+++ Cpp/CppPlayground/CppPlayground/threading.hpp
@@ -0,0 +1,14 @@
+//
+//  threading.hpp
+//  CppPlayground
+//
+//  Created by rico on 18.03.18.
+//  Copyright © 2018 rico. All rights reserved.
+//
+
+#ifndef threading_hpp
+#define threading_hpp
+
+#include <stdio.h>
+
+#endif /* threading_hpp */
