cc3306e3a6a4a6f4868d70b4ad1420e5ef0e1864
robot42 <github@ehuettig42.de>
2018-07-22 21:00:19 +0200

    test

--- Cpp/CppPlayground/main.cpp
+++ Cpp/CppPlayground/main.cpp
@@ -4,6 +4,7 @@
 #include "Concurrency/AsyncTaskProcessorFactory.h"
 #include "Concurrency/Task.h"
 #include <iostream>
+#include "boost/format.hpp"
 #include <pplawait.h>
 
 using namespace std;
@@ -27,28 +28,39 @@ private:
 	int delay;
 };
 
-
+concurrency::task<int> Save(int i)
+{
+	this_thread::sleep_for(std::chrono::seconds(i));
+	std::cout << boost::format("%1%") % i << endl;
+	co_return i;
+}
 
 future<void> test(shared_ptr<ITaskProcessorFactory> factory)
 {
+	std::cout << "Staring in thread " << this_thread::get_id() << endl;
+
+	// auto result = co_await Save(10);
+
 	auto p1 = factory->CreateTaskProcessor();
 	auto p2 = factory->CreateTaskProcessor();
 	auto task1 = make_shared<Waiter>(1);
 	auto task2 = make_shared<Waiter>(2);
-	auto task3 = make_shared<Waiter>(4);
+	auto task3 = make_shared<Waiter>(3);
 
-	p1->Enqueue(task3);
 	
 	p2->Enqueue(task1);
 	auto result1 = co_await task1->Future();
 	cout << "Waiter " << result1 << " returned" << endl;
 
+	p1->Enqueue(task3);
+	auto result3 = co_await task3->Future();
+	cout << "Waiter " << result3 << " returned" << endl;
+
 	p2->Enqueue(task2);
 	auto result2 = co_await task2->Future();
 	cout << "Waiter " << result2 << " returned" << endl;
 
-	auto result3 = co_await task3->Future();
-	cout << "Waiter " << result3 << " returned" << endl;
+	std::cout << "Ending in thread " << this_thread::get_id() << endl;
 }
 
 int main(int argc, const char *argv[])
