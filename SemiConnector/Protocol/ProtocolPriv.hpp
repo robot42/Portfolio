72d7d86d437e6c7ce5a7434c394279cb629f64dd
robot42 <github@ehuettig42.de>
2019-10-06 21:00:27 +0200

    cleanup

---
+++ SemiConnector/Protocol/ProtocolPriv.hpp
@@ -0,0 +1,18 @@
+//
+//  ProtocolPriv.hpp
+//  Protocol
+//
+//  Created by rico on 28.09.19.
+//  Copyright © 2019 rico. All rights reserved.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class ProtocolPriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
